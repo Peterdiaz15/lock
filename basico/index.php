1e46a1aef403f7adc28c3927643ff83740341f50
Peter <pedrodiaz15.15@gmail.com>
2020-11-29 00:52:06 -0600

    ventana basico

--- basico/index.php
+++ basico/index.php
@@ -15,7 +15,7 @@
  ?>	
 <div class="row">
     <div class="col-12" id="banner">
-        <img src="../img/banner-basico.png" id="img-compra">
+        <img src="../img/banner-basico.png" id="img-compra" width="100%">
     </div>
 </div>
 <div class="row" id="textos">
@@ -75,7 +75,73 @@
         <td>Honorarios Quirúrgicos por reembolso</td>
         <td>Gua +75 %</td>
       </tr>
+      <tr>
+        <td>Suma Asegurada</td>
+        <td>$5,000,000</td>
+      </tr>
+      <tr>
+        <td>Deducible</td>
+        <td>" SMGM (7,393)</td>
+      </tr>
+      <tr>
+        <td>Coaseguro</td>
+        <td>10%</td>
+      </tr>
+      <tr>
+        <td>Tope de Coaseguro</td>
+        <td>$ 30,000</td>
+      </tr>
+      <tr>
+        <td>Derecho de Conversión a Individual</td>
+        <td>No amparado</td>
+      </tr>
+      <tr>
+        <td>Asistencias *</td>
+        <td class="txt-espaciado">Médico On Line (Servicios Ilimitados)<br>
+        Ambulancia (2 eventos anuales)<br>Consultas con Pediatras (2 anuales sin costo)<br>Consultas Médicos Generales (2 anuales sin costo)<br>Descuentos Médicos<br>Check Up Básico (1 evento anual)</td>
+      </tr>
+      <tr>
+        <td>Forma de Pago</td>
+        <td>Mensual (Tarjeta de Crédito o Débito)</td>
+      </tr>
+      <tr>
+        <td>Mínimo de concentración</td>
+        <td>15% de la Población Titular</td>
+      </tr>
     </table>
+  </div>
+  <div class="col-12 display-flex espacio" id="formulario">
+    <form class="margin-auto">
+      <div class="form-row">
+        <div class="col-sm-6 col-lg-3 padding-right">
+          <label for="inputEmail4">Edad</label>
+          <select id="inputState" class="form-control">
+            <option selected>Choose...</option>
+            <option>...</option>
+          </select>
+        </div>
+        <div class="col-sm-6 col-lg-3 padding-right">
+          <label for="inputEmail4">Sexo</label>
+          <select id="inputState" class="form-control">
+            <option>Hombre</option>
+            <option>Mujer</option>
+          </select>
+        </div>
+        <div class="col-sm-6 col-lg-3 padding-right">
+          <label for="inputEmail4">Costo Final</label>
+          <input type="email" class="form-control" id="inputEmail4">
+        </div>
+        <div class="col-sm-6 col-lg-3 display-flex" id="adquirir">
+          <button type="button" class="btn btn-primary margin-auto">ADQUIRIR</button>
+        </div>
+      </div>
+    </form>
+  </div>
+  <div class="col-12 display-flex espacio">
+    <small class="margin-auto texto-centrado">
+      <b>No ampara: Complementos, Antigüedad, eliminación de periodos de espera, Dividendos, Facturas Individualizadas.
+      Beneficio no portable.<br>*Amparado por Iké Asistencias (Aplican condiciones de acuerdo a sus condiciones Generales).<br>La divulgación incompleta y parcial de los hechos importantes que puedan influir en la apreciación del riesgo, incurrirá en la falsa declaración<br>/no divulgación de información;  hecho que facultará a la(s) compañía(s) de seguros para considerar la invalidez de la cotización proporcionada o el contrato de seguro emitido.</b>
+    </small>
   </div>
 </div>
 
