97d0487f20a8026641a2d67a0453e758a5d5a02a
Juanjo <juan.cisneros3546@gmail.com>
2020-11-29 01:11:36 -0600

    Completo escudo dental

---
+++ Dental/index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8;IE=edge"/>
+	<title>Lockton</title>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/dental.css">
+	<link rel="stylesheet" href="../css/generales.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<?php include '../php/menu.php' ?>
+		<div class="row">
+			<div class="col-12">
+				<img src="../img/dental-banner.jpeg" width="100%">
+			</div>
+		</div>
+		<div class="row" id="textos">
+			<div class="col-sm-12 col-lg-6 display-flex">
+				<div class="margin-auto row">
+					<div class="col-12">
+						<p class="whiteT">Categorías: <span class="blueT">Colaboradores</span></p>
+					</div>
+					<div class="col-12">
+						<p class="whiteT">Asistenciadora: <span class="blueT">Iké Asistencia</span></p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-12 col-lg-6 display-flex">
+				<div class="margin-auto row">
+					<div class="col-12">
+						<p class="whiteT">Aseguradora: <span class="blueT">Dentegra</span></p>
+					</div>
+					<div class="col-12 display-flex">
+						<p class="whiteT">Elegibilidad: </p><span class="blueT">Titular, Cónyuge, Padres (siempre y cuando dependa <br>
+						económicamente de éste) e hijos</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row" id="contenedorText">
+			<div class="col-12 display-flex text-center espacio">
+				<p class="margin-auto"><strong>Escudo Dental es el programa de Asistencias y Seguros de Salud Dental para ti y tu familia.</strong><br> Tu y tu familia contarán con la cobertura de Seguros de Gastos Médicos ante Emergencias dentales a nivel nacional. <br>
+				Tratamientos de endodoncia, procedimientos quirúrgicos, sesiones de limpieza y seguimiento, entre otros.</p>
+			</div>
+			<div class="col-12 display-flex espacio2">
+				<div class="margin-auto iden">
+					<table class="table text-center">
+						<thead class="thead-dark">
+							<th  rowspan="2">DESCRIPCION</th>
+
+							<th colspan="2">COPAGO A CARGO DEL ASEGURADO</th>
+							<tr>
+								<th>JADE BASE</th>
+								<th>CUARZO SEMI</th>
+							</tr>
+					</thead>
+					<tbody id="tabla" class="text-center">
+						<tr>
+							<td>Urgencias Dentales Nacionales</td>
+							<td>0%</td>
+							<td>0%</td>
+						</tr>
+						<tr>
+							<td><strong>Diagnostico y Prevencion</strong><br> (Evaluacion oral,Rayos X, Limpieza, Fluor y Selladores)</td>
+							<td>20%</td>
+							<td>0%</td>
+						</tr>
+						<tr>
+							<td><strong>Restaurativo Basico</strong><br> (Tratamiento con Amalgamas y Resinas)</td>
+							<td>20%</td>
+							<td>20%</td>
+						</tr>
+						<tr>
+							<td><strong>Procedimientos Quirurgicos</strong><br> (Extrasiones Simples y otros procedimientos menores)</td>
+							<td>20%</td>
+							<td>20%</td>
+						</tr>
+						<tr>
+							<td><strong>Endodoncias</strong><br> (Remocion de nervio dental)</td>
+							<td>20%</td>
+							<td>20%</td>
+						</tr>
+						<tr>
+							<td><strong>Remocion de terceros Molares y Procedimientos Quirugicos Complejos</strong><br> (Remocion de nervio dental)</td>
+							<td>20%</td>
+							<td>20%</td>
+						</tr>
+						<tr>
+							<td><strong>Periodoncia</strong><br> (Tratamiento de enfermedad de encia y hueso)</td>
+							<td>No Incluido <br>Descuento de 20%</td>
+							<td>20%</td>
+						</tr>
+						<tr>
+							<td><strong>Coronas/Prostodoncia</strong><br> (Coronas, puentes y dentaduras parciales y completas)</td>
+							<td>No Incluido <br>Descuento de 20%</td>
+							<td>20%</td>
+						</tr>
+						<tr>
+							<td>Limite Anual por personas</td>
+							<td>$35,000</td>
+							<td>$40,000</td>
+						</tr>
+						<tr class="thead-dark">
+							<th>Prima Total Anual por Asegurado</th>
+							<th>$1,670</th>
+							<th>$1,992</th>
+						</tr>
+						<tr id="ultimaFila">
+							<td class="fila"></td>
+							<td class="fila"><button class="btn btn-primary">ADQUIRIR</button></td>
+							<td class="fila"><button class="btn btn-primary">ADQUIRIR</button></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="col-12 display-flex espacio">
+		 <div class="margin-auto iden2">
+		 	<ul>
+              <li>Elegibilidad<br>Titular, Cónyuge, Hijos y Padres siempre y cuando dependa económicamente de éste.</li><br>
+              <li>Edad de Aceptación<br>Titular, Cónyuge y Padres sin limite de edad<br>Hijos: hasta los 24 años.</li><br>
+              <li>Para la contratación de este plan se requieren al menos 10 Titulares.</li>
+              <li>Para contratar este plan a dependientes es necesario que el Titular cuente con él.</li><br>
+              <li>La prima se cobrará en forma individual por cada persona asegurada.</li>
+              <li>Se considera forma de pago Anual.</li>
+              <li>La prima total es anual e incluye IVA, derecho de póliza.</li>
+              <li>Forma de Pago: Tarjeta de Crédito o Débito.</li>
+            </ul>
+		 </div>
+		</div>
+		<div class="col-12 display-flex">
+		 <div class="margin-auto iden2">
+		  <p>Consideraciones Importantes <br>
+			Los dependientes deberán inscribirse al programa al mismo tiempo que el Titular. No se permite la inscripción de solo algunos de los dependientes.
+			Para nuevos dependientes (hijos nacidos durante la vigencia, o que el Titular haya contraído matrimonio) deberán inscribirse el primer día del mes siguiente a su elegibilidad como dependientes.
+			Para nuevos empleados, se podrán dar de alta junto con todos sus dependientes en un plazo no mayor a los 30 días posteriores a su contratación.
+			No hay cancelación anticipada del seguro. En caso de baja del Titular de la empresa se le deberá descontar el importe de la prima del seguro de su finiquito y podrá continuar con la cobertura hasta el fin de vigencia.
+			El asegurado recibirá por parte de Dentegra: Certificado Digital.
+          </p>
+		 </div>
+		</div>
+		<?php include '../php/footer.php' ?>
+	</div>
+</div>
+</body>
+</html>
